44b640437d30a6506c84ebe165ee89602264cefd
tchapi <regbasket+github@gmail.com>
2019-11-15 19:16:20 +0100

    Use published at correctly

--- src/Entity/NewsItem.php
+++ src/Entity/NewsItem.php
@@ -18,12 +18,12 @@ class NewsItem
     private $id;
 
     /**
-     * @ORM\Column(type="date")
+     * @ORM\Column(type="datetime")
      */
     private $createdAt;
 
     /**
-     * @ORM\Column(type="date", nullable=true)
+     * @ORM\Column(type="datetime", nullable=true)
      */
     private $publishedAt;
 
